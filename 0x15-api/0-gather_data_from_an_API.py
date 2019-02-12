5171c837b30354761822099e689a76b1c39e6e1d
Paul Lave <pmlave@gmail.com>
2019-02-12 01:12:19 +0000

    Attempting to resolve missing check with different printing format

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,29 +9,27 @@ def main():
         employee_id = sys.argv[1]
     except:
         return
-    try:
-        par = {'id': employee_id}
-        emp = requests.get('https://jsonplaceholder.typicode.com/users',
-                           params=par)
-        emp = emp.json()
+    par = {'id': employee_id}
+    emp = requests.get('https://jsonplaceholder.typicode.com/users',
+                       params=par)
+    emp = emp.json()
 
-        par = {'userId': employee_id}
-        todo = requests.get('https://jsonplaceholder.typicode.com/todos',
-                            params=par)
-        todo = todo.json()
-        total = 0
-        finished = 0
-        for thing in todo:
-            if thing.get('completed') is True:
-                finished += 1
-            total += 1
-        print("Employee {} is done with tasks({:d}/{:d}):".format(
-            emp[0].get('name'), finished, total))
-        for thing in todo:
-            if thing.get('completed') is True:
-                print("     {}".format(thing.get('title')))
-    except Exception as e:
-        print(e)
+    par = {'userId': employee_id}
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos',
+                        params=par)
+    todo = todo.json()
+    total = 0
+    finished = 0
+    complete = []
+    for thing in todo:
+        if thing.get('completed') is True:
+            finished += 1
+            complete.append(thing.get('title'))
+        total += 1
+    print("Employee {} is done with tasks({}/{}):".format(
+        emp[0].get('name'), finished, total))
+    for thing in complete:
+        print("\t {}".format(thing))
 
 if __name__ == "__main__":
     main()
